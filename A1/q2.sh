5e04d5d113365df00d0001b54f66d7626edebf4d
Anirudh Mungre <anirudh.mungre16@gmail.com>
2018-10-03 12:22:20 -0400

    answer q2 with delay 5 seconds

---
+++ A1/q2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# A shell script to write the current time into
+# the file "sampling.txt" every 5 seconds until
+# user interupts
+
+while :
+do
+    touch sampling.txt
+    date > sampling.txt
+    sleep 5
+done
