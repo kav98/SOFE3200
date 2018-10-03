900df0111d1afc0e033bdf92169a2af008d6854c
Anirudh Mungre <anirudh.mungre16@gmail.com>
2018-10-03 16:14:44 -0400

    finish code segment for 4c

--- A1/q4.sh
+++ A1/q4.sh
@@ -5,3 +5,10 @@
 #QUESTION 4a
 echo "QUESTION 4a -"
 wc -l ATaleofTwoCities.txt AliceInWonderland.txt
+
+#QUESTION 4b
+printf "\nQUESTION 4b -\n"
+grep -oe "London" -e "Paris" ATaleofTwoCities.txt | wc -l
+
+#QUESTION 4c
+printf "\nQUESTION 4c -\n"
