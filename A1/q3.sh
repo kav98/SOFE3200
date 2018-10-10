6b4d9d1989262f4e2d2e6dee2be1e15899227c5b
Anirudh Mungre <anirudh.mungre16@gmail.com>
2018-10-10 15:51:25 -0400

    experimenting with filename conventions

--- A1/q3.sh
+++ A1/q3.sh
@@ -2,3 +2,36 @@
 
 # 
 
+touch date 
+read -p "Please input your name: " name
+start=`date +%s`
+input=()
+for i in {1..5};
+do
+    case $i in
+    1)
+        printf "Can pigs fly?\n0: TRUE\n1: FALSE\n"
+        ans1 = "0"
+    ;;
+    2)
+        printf "Do most humans have feet?\n0: TRUE\n1: FALSE\n"
+    ;;
+    3)
+        printf "Are lions vegan?\n0: TRUE\n1: FALSE\n"
+    ;;
+    4)
+        printf "Are dogs and cats related?\n0: TRUE\n1: FALSE\n"
+    ;;
+    5)
+        printf "Is the TA going to give me 100%%?\n0: TRUE\n1: FALSE\n"
+    ;;
+    esac
+    read -p "Please input answer: " choice
+    input+=choice
+done
+end=`date +%s`
+runtime=$((end-start))
+filename=$ date +"%D"
+filename+="-"$runtime"-"
+filename+=$name".txt"
+touch /q3Records $filename
