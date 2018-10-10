9e0a6dc2fe4f3e632cfeb4c8d7fc11b6c48896a3
Anirudh Mungre <anirudh.mungre16@gmail.com>
2018-10-10 14:23:34 -0400

    formatting for 4c tinkering with 4d

--- A1/q4.sh
+++ A1/q4.sh
@@ -4,15 +4,24 @@
 
 #QUESTION 4a
 echo "QUESTION 4a -"
+printf "Number of lines across both files:\n"
 wc -l ATaleofTwoCities.txt AliceInWonderland.txt
 
 #QUESTION 4b
 printf "\nQUESTION 4b -\n"
-grep -oe "London" -e "Paris" ATaleofTwoCities.txt | wc -l
+printf "Number of times \'London\' and \'Paris\' appear: "
+grep -oe "London" -e "resetParis" ATaleofTwoCities.txt | wc -l
 
 #QUESTION 4c
 printf "\nQUESTION 4c -\n"
+printf "Number of Vowels: "
 grep -o [aeiouAEIOU] ATaleofTwoCities.txt AliceInWonderland.txt | wc -l
+printf "Number of times \'the\' appears: "
 grep -o "the" ATaleofTwoCities.txt AliceInWonderland.txt | wc -l
 sed 's/the/ABC/g' ATaleofTwoCities.txt > ATaleofTwoCitiesABC.txt
 sed 's/the/ABC/g' AliceInWonderland.txt > AliceInWonderlandABC.txt
+printf "New files with \'the\' replaced with \'ABC' created.\n"
+
+#QUESTION 4d
+printf "\nQUESTION 4d -\n"
+grep -o . ATaleofTwoCities.txt AliceInWonderland.txt | sort -u
