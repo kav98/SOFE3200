c60bd17ec7c3d5903ccbb028b858d3cd540f9cf1
Anirudh Mungre <anirudh.mungre16@gmail.com>
2018-10-10 13:45:23 -0400

    finish 4c and create final text files with ABC

--- A1/q4.sh
+++ A1/q4.sh
@@ -12,4 +12,7 @@ grep -oe "London" -e "Paris" ATaleofTwoCities.txt | wc -l
 
 #QUESTION 4c
 printf "\nQUESTION 4c -\n"
-printf ("%d" % grep -o [aeiouAEIOU] ATaleofTwoCities.txt AliceInWonderland.txt | wc -l)
+grep -o [aeiouAEIOU] ATaleofTwoCities.txt AliceInWonderland.txt | wc -l
+grep -o "the" ATaleofTwoCities.txt AliceInWonderland.txt | wc -l
+sed 's/the/ABC/g' ATaleofTwoCities.txt > ATaleofTwoCitiesABC.txt
+sed 's/the/ABC/g' AliceInWonderland.txt > AliceInWonderlandABC.txt
