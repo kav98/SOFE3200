49d18f30ed0f8fde3b529af001ca33ebc3927e26
Anirudh Mungre <anirudh.mungre16@gmail.com>
2018-11-01 21:23:11 -0400

    finish task 1

--- Lab3/task1.c
+++ Lab3/task1.c
@@ -2,19 +2,32 @@
 #include <stdlib.h>
 int main ()
 {
-    int num;
-    FILE *fptr;
-
-    if ((fptr = fopen("input.txt","r+")) == NULL){
-        printf("Error! opening file\n");
-        // Program exits if the file pointer returns NULL.
-        exit(1);
+    FILE* file = fopen("input.txt", "r"); //read file
+    while (1){
+        if (file != NULL){
+            break; //file exists so break
+        }
+        else{
+            printf("File does not exist!\n"); //File not found if NULL
+            exit(12); //so exit
+        }
     }
 
-    fscanf(fptr,"%d", &num);
+    FILE* newFile = fopen("Heaven.txt", "w");
 
-    printf("Value of n=%d", num);
-    fclose(fptr); 
+    char line[512];
+    char eLine[512]; //edited line
 
-    return 0;
+    while (fgets(line, sizeof(line), file)) {
+        printf("%s", line);
+        for (int i = 0 ; i<sizeof(line) ; i++){ //iterate by character
+            if (line[i] == 'a'){eLine[i] = 'b';} //if the char is 'a' change to 'b'
+            else{eLine[i]=line[i];} // else copy char
+        }
+        fprintf(newFile, "%s", eLine); // print new line to new file "heaven.txt"
+    }
+    fclose(file);
+    fclose(newFile);
+    //close files
+    return -0; //returning nega zero because we are sad
 }
